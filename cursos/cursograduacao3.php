2306581c489e8797ddd26c38b5c3bc6a1ac4f281
Nyelson Gomes Barbosa <nyelsonbarbosa@hotmail.com>
2015-06-05 17:30:23 -0300

    Cursos v1.0
    Graduações, Pós graduações, e cursos intensivos.

---
+++ cursos/cursograduacao3.php
@@ -0,0 +1,96 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <meta name="author" content="Tiago Henrique, Yasmin Farias, Nyelson Gomes, Renan Oliveira, Ramon Portela, Roberto Souza" /> 
+  	<meta name="keywords" content="faculdade, alunos, home" />
+  	<meta name="description" content="Sistema de Gestão Acadêmica, Avaliações e Administração de Curso em uma Instituição." />
+    <title>SGA | Gestão da Tecnologia da Informação</title>
+    
+    <!-- include com os scripts e CSS do SGA -->
+    <? include("../includes/server/include-css-js-favicon.php"); ?> 
+</head>
+
+    <!-- include com o header, botões sociais, Procurar, logo e Menu -->
+    <? include("../includes/server/include-header.php"); ?> 
+    
+    <section id="section-sobre-graduacaogti" >
+        <div class="container">
+        	<div class="center wow fadeInDown">
+            	<h2>Gestão da Tecnologia da Informação</h2>
+            </div> <!-- center wow fadeInDown -->
+            <div class="wow fadeInDown">
+					<div class="col-sm-12">
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce luctus elit dui, id sodales nibh consectetur non. Nullam rutrum congue purus et luctus. Proin rhoncus in mauris sed hendrerit. Duis congue eros massa, eget tristique eros scelerisque id. Nunc at neque mattis, semper nibh et, accumsan leo. Cras quis vulputate ante. Donec lorem dui, tincidunt id efficitur ac, pretium ut nisl. Etiam lacus turpis, molestie id lorem ut, auctor pulvinar erat. Donec sapien dolor, lacinia maximus tortor consequat, dignissim tristique ligula. Duis mattis est eu magna laoreet cursus. Maecenas sit amet congue dui. Mauris consectetur orci lacus, ut porta augue rhoncus quis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut blandit eros non erat bibendum, vel sollicitudin tortor aliquam. In egestas ex quam, vel lobortis dui ullamcorper non.<br/>
+Aliquam eu lorem volutpat, faucibus massa ut, pharetra nunc. Aenean sagittis elit ac consectetur maximus. Nunc pulvinar quam rutrum sodales facilisis. Fusce tortor nulla, fermentum ut sem non, fermentum gravida risus. Proin auctor ipsum non tortor sodales ornare. Praesent imperdiet neque non ipsum iaculis lobortis. Sed sodales quis nibh a commodo. In lacinia arcu nulla, ac ornare urna iaculis vitae. Vivamus condimentum risus nisi, elementum condimentum odio volutpat fringilla. Aenean eget auctor risus. Ut ullamcorper aliquam diam, in rhoncus orci tincidunt ac.<br/>
+Nam iaculis dignissim dolor a mollis. Cras rutrum tempor libero, vitae vestibulum nulla hendrerit eu. Aliquam elit mi, tempus sed consequat nec, convallis ac elit. Vestibulum sed enim at tellus faucibus pretium vitae vel dolor. Aliquam erat volutpat. Duis scelerisque mauris id vehicula pharetra. Nulla tempus urna turpis. Nam posuere tellus purus, et egestas neque rutrum sed. Aliquam velit mauris, tempus ac arcu quis, eleifend sodales ipsum. Nullam accumsan diam vitae vestibulum bibendum. Suspendisse sed nisi sit amet justo dignissim commodo at vitae eros. Quisque faucibus blandit nibh eget blandit. Aliquam ac nisl nibh. Integer feugiat odio at erat elementum, varius vestibulum libero consequat. Aenean porttitor lorem sed felis lacinia, in finibus elit vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+						</p>
+					</div> <!-- col-sm-12 -->
+				</div> <!-- wow fadeInDown -->
+			</div> <!-- container -->
+		</section> <!-- section-sobre-sga -->
+    
+    <section id="section-sobre-gti" >
+	    	<div class="container">
+            	<div class="wow fadeInDown">
+                    <div class="row">
+                        <div class="col-md-4 col-sm-6">
+                            <div class="in-press press-wired">
+                                <a href="#">Visando expandir a área de atuação de nossa empresa, não tive dúvidas em escolher o Infnet para adquirir o conhecimento necessário na Gestão da Segurança e Serviços de TI.</a>
+                            </div> <!-- in-press press-wired -->
+                        </div> <!-- col-md-4 col-sm-6 -->
+                        <div class="col-md-4 col-sm-6">
+                            <div class="in-press press-mashable">
+                                <a href="#">Um ótimo curso de Pós-graduação voltado ao mercado, com uma equipe de professores experientes que trabalham em grandes empresas e com isso temos exemplos reais na sala aula. Tenho certeza que fiz a escolha certa.</a>
+                            </div> <!-- in-press press-mashable -->
+                        </div> <!-- col-md-4 col-sm-6 -->
+                        <div class="col-md-4 col-sm-6">
+                            <div class="in-press press-techcrunch">
+                                <a href="#">Encontrei um ambiente de aprendizado que julgava não existir no Brasil. As instalações, o material de apoio, e, principalmente, as pessoas - professores e funcionários - são de nível internacional.</a>
+                            </div> <!-- in-press press-techcrunch -->
+                        </div> <!-- col-md-4 col-sm-6 -->
+                    </div> <!-- row -->
+                  </div> <!-- wow fadeInDown -->
+			</div> <!-- container -->
+	</section> <!-- section-sobre-opnioes -->
+    
+    <section id="section-sobre-slide-gti">
+        <div class="container">
+			<div class="center wow fadeInDown">
+                <div id="about-slider">
+                    <div id="carousel-slider" class="carousel slide" data-ride="carousel">
+                        <ol class="carousel-indicators visible-xs">
+                            <li data-target="#carousel-slider" data-slide-to="0" class="active"></li>
+                            <li data-target="#carousel-slider" data-slide-to="1"></li>
+                            <li data-target="#carousel-slider" data-slide-to="2"></li>
+                        </ol>
+    
+                        <div class="carousel-inner">
+                            <div id="gti-slide-img1" class="item active">
+                                <img src="/images/sobre/img1.jpg" class="img-responsive" alt=""> 
+                           </div> <!-- gti-slide-img1 -->
+                           <div id="gti-slide-img2" class="item">
+                                <img src="../images/sobre/img2.jpg" class="img-responsive" alt=""> 
+                           </div> <!-- gti-slide-img2 -->
+                           <div id="gti-slide-img3" class="item">
+                                <img src="../images/sobre/img3.jpg" class="img-responsive" alt=""> 
+                           </div> <!-- gti-slide-img3 --> 
+                        </div> <!-- carousel-inner -->
+                        
+                        <a class="left carousel-control hidden-xs" href="#carousel-slider" data-slide="prev">
+                            <i class="fa fa-angle-left"></i> 
+                        </a>
+                        
+                        <a class=" right carousel-control hidden-xs"href="#carousel-slider" data-slide="next">
+                            <i class="fa fa-angle-right"></i> 
+                        </a>
+                    </div> <!-- carousel-slider -->
+                </div><!-- about-slider -->
+			</div><!-- section -->
+		</div><!-- container -->
+    </section><!--section-sobre-slide -->
+    
+    <!-- include com o section 'bottom' e o footer -->
+    <? include("../includes/server/include-footer.php"); ?> 
+</body>
+</html>
