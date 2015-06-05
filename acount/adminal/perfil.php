82e6f2e30062fa9ce944557e2e9f45c35f3cc83c
tiagohs <tiago.silva.93@hotmail.com>
2015-06-04 23:23:37 -0300

    v2.9
    Perfil Aluno Refatorado

--- acount/adminal/perfil.php
+++ acount/adminal/perfil.php
@@ -24,129 +24,51 @@
 				<li><a href="/index.php" title="Página Inicial da SGA" ><span class="glyphicon glyphicon-home"></span></a></li>
 				<li><?=TITULO?></li>
 			</ol>
-		</div><!--/.row-->
+		</div><!-- row -->
 		
 		<div class="row">
 			<div class="col-lg-12">
 				<h1 class="page-header"><?=TITULO?></h1>
 			</div>
-		</div><!--/.row-->
+		</div><!-- row -->
 		
         <div class="col-md-6">
-<<<<<<< HEAD
-            <div class="panel panel-default"> 
-                <div class="panel-heading">Dados Pessoais</div> <!-- panel panel-default -->
+            <div class="panel panel-default">
+                <div class="panel-heading">Dados Pessoais</div> <!-- panel-heading -->
                 <div class="table-responsive panel-body">
                     <table class="table table-striped">
                         <tbody>
                             <td class="success">Email de Acesso: </td>
-                            <td>tiago.silva.93@hotmail.com</td>
+                            <td><?= $_SESSION['alEmail']?></td>
                         </tbody>
                         <tbody>
                             <td class="success">Nome: </td>
-                            <td>Tiago Henrique</td>
+                            <td><?= $_SESSION['alNome']?></td>
                         </tbody>
                         <tbody>
                             <td class="success">Nascimento</td>
-                            <td>08/11/1993</td>
+                            <td><?= $_SESSION['alDataNascimento']?></td>
                         </tbody>
                         <tbody>
                             <td class="success">CPF</td>
-                            <td>424.254.548-78</td>
+                            <td><?= $_SESSION['alCpf']?></td>
                         </tbody>
                         <tbody>
                             <td class="success">Sexo</td>
-                            <td>Masculino</td>
+                            <td><?= $_SESSION['alSexo']?></td>
                         </tbody>
                         <tbody>
                             <td class="success">Telefone</td>
-                            <td>3879-6521</td>
+                            <td><?= $_SESSION['alTelefone']?></td>
                         </tbody>
                         <tbody>
                             <td class="success">Celular</td>
-                            <td>98782-8974</td>
+                            <td><?= $_SESSION['alCelular']?></td>
                         </tbody>
                     </table>
                 </div> <!-- table-responsive panel-body -->
             </div> <!-- panel panel-default -->
         </div> <!-- col-md-6 -->
-        
-        <div class="col-md-6">
-            <div class="panel panel-default"> 
-                <div class="panel-heading">Endereço</div> <!-- panel panel-default -->
-                <div class="table-responsive panel-body">
-                    <table class="table table-striped">
-                        <tbody>
-                            <td class="success">CEP: </td>
-                            <td>21911-200</td>
-                        </tbody>
-                        <tbody>
-                            <td class="success">Tipo de Logradouro: </td>
-                            <td>Casa</td>
-                        </tbody>
-                        <tbody>
-                            <td class="success">Logradouro: </td>
-                            <td>Rua Pio Dutra - 140</td>
-                        </tbody>
-                        <tbody>
-                            <td class="success">Complemento: </td>
-                            <td>--</td>
-                        </tbody>
-                        <tbody>
-                            <td class="success">Bairro: </td>
-                            <td>Ilha do Governador</td>
-                        </tbody>
-                        <tbody>
-                            <td class="success">Cidade: </td>
-                            <td>Rio de janeiro</td>
-                        </tbody>
-                        <tbody>
-                            <td class="success">Estado: </td>
-                            <td>Rio de janeiro</td>
-                        </tbody>
-                    </table>
-                </div> <!-- table-responsive panel-body -->
-            </div> <!-- panel panel-default -->
-        </div> <!-- col-md-6 -->
-	</section> <!-- main -->
-=======
-            <div class="panel panel-default">
-                <div class="panel-heading">Dados Pessoais</div>
-                    <div class="table-responsive panel-body">
-                        <table class="table table-striped">
-                            <tbody>
-                                <td class="success">Email de Acesso: </td>
-                                <td><?= $_SESSION['alEmail']?></td>
-                            </tbody>
-                            <tbody>
-                                <td class="success">Nome: </td>
-                                <td><?= $_SESSION['alNome']?></td>
-                            </tbody>
-                            <tbody>
-                                <td class="success">Nascimento</td>
-                                <td><?= $_SESSION['alDataNascimento']?></td>
-                            </tbody>
-                            <tbody>
-                                <td class="success">CPF</td>
-                                <td><?= $_SESSION['alCpf']?></td>
-                            </tbody>
-                            <tbody>
-                                <td class="success">Sexo</td>
-                                <td><?= $_SESSION['alSexo']?></td>
-                            </tbody>
-                            <tbody>
-                                <td class="success">Telefone</td>
-                                <td><?= $_SESSION['alTelefone']?></td>
-                            </tbody>
-                            <tbody>
-                                <td class="success">Celular</td>
-                                <td><?= $_SESSION['alCelular']?></td>
-                            </tbody>
-                        </table>
-                    </div>
-                  </div>
-            </div>
-        </div>
         
         <div class="col-md-6">
             <div class="panel panel-default">
@@ -183,13 +105,11 @@
                                 <td><?= $_SESSION['alEstado']?></td>
                             </tbody>
                         </table>
-                    </div>
-                  </div>
-            </div>
-        </div>
-        
+                    <</div> <!-- table-responsive panel-body -->
+                </div> <!-- panel panel-default -->
+            </div> <!-- col-md-6 -->
 		
-	</section>	<!--/.main-->
+	</section> <!-- main -->
 >>>>>>> origin/master
 </body>
 
