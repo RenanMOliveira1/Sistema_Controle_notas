e0259a0d263c8817ec8cbb241c5e94e7abb147b2
Ramon Portela <portela.ramon96@gmail.com>
2015-06-04 23:16:30 -0300

    Revert "V2.8"
    This reverts commit cfbca6709dd7b5b3edf60aa23799e9319f8c5e39.

--- acount/adminal/perfil.php
+++ acount/adminal/perfil.php
@@ -33,6 +33,83 @@
 		</div><!--/.row-->
 		
         <div class="col-md-6">
+<<<<<<< HEAD
+            <div class="panel panel-default"> 
+                <div class="panel-heading">Dados Pessoais</div> <!-- panel panel-default -->
+                <div class="table-responsive panel-body">
+                    <table class="table table-striped">
+                        <tbody>
+                            <td class="success">Email de Acesso: </td>
+                            <td>tiago.silva.93@hotmail.com</td>
+                        </tbody>
+                        <tbody>
+                            <td class="success">Nome: </td>
+                            <td>Tiago Henrique</td>
+                        </tbody>
+                        <tbody>
+                            <td class="success">Nascimento</td>
+                            <td>08/11/1993</td>
+                        </tbody>
+                        <tbody>
+                            <td class="success">CPF</td>
+                            <td>424.254.548-78</td>
+                        </tbody>
+                        <tbody>
+                            <td class="success">Sexo</td>
+                            <td>Masculino</td>
+                        </tbody>
+                        <tbody>
+                            <td class="success">Telefone</td>
+                            <td>3879-6521</td>
+                        </tbody>
+                        <tbody>
+                            <td class="success">Celular</td>
+                            <td>98782-8974</td>
+                        </tbody>
+                    </table>
+                </div> <!-- table-responsive panel-body -->
+            </div> <!-- panel panel-default -->
+        </div> <!-- col-md-6 -->
+        
+        <div class="col-md-6">
+            <div class="panel panel-default"> 
+                <div class="panel-heading">Endereço</div> <!-- panel panel-default -->
+                <div class="table-responsive panel-body">
+                    <table class="table table-striped">
+                        <tbody>
+                            <td class="success">CEP: </td>
+                            <td>21911-200</td>
+                        </tbody>
+                        <tbody>
+                            <td class="success">Tipo de Logradouro: </td>
+                            <td>Casa</td>
+                        </tbody>
+                        <tbody>
+                            <td class="success">Logradouro: </td>
+                            <td>Rua Pio Dutra - 140</td>
+                        </tbody>
+                        <tbody>
+                            <td class="success">Complemento: </td>
+                            <td>--</td>
+                        </tbody>
+                        <tbody>
+                            <td class="success">Bairro: </td>
+                            <td>Ilha do Governador</td>
+                        </tbody>
+                        <tbody>
+                            <td class="success">Cidade: </td>
+                            <td>Rio de janeiro</td>
+                        </tbody>
+                        <tbody>
+                            <td class="success">Estado: </td>
+                            <td>Rio de janeiro</td>
+                        </tbody>
+                    </table>
+                </div> <!-- table-responsive panel-body -->
+            </div> <!-- panel panel-default -->
+        </div> <!-- col-md-6 -->
+	</section> <!-- main -->
+=======
             <div class="panel panel-default">
                 <div class="panel-heading">Dados Pessoais</div>
                     <div class="table-responsive panel-body">
