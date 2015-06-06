ea2de910ecbd40e320693b5a3378a85bc9b573e2
Nyelson Gomes Barbosa <nyelsonbarbosa@hotmail.com>
2015-06-06 10:39:24 -0300

    Servicos v1.0
    Servicos de Carreiras, concursos e etc

---
+++ servicos/carreiras.php
@@ -0,0 +1,47 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <meta name="author" content="Tiago Henrique, Yasmin Farias, Nyelson Gomes, Renan Oliveira, Ramon Portela, Roberto Souza" /> 
+  	<meta name="keywords" content="faculdade, alunos, home" />
+  	<meta name="description" content="Sistema de Gestão Acadêmica, Avaliações e Administração de Curso em uma Instituição." />
+    <title>SGA | Sobre a Instituição</title>
+    
+    <!-- include com os scripts e CSS do SGA -->
+    <? include("../includes/server/include-css-js-favicon.php"); ?> 
+</head>
+
+    <!-- include com o header, botões sociais, Procurar, logo e Menu -->
+    <? include("../includes/server/include-header.php"); ?> 
+    
+    <section id="section-sobre-sga" >
+        <div class="container">
+        	<div class="center wow fadeInDown">
+            	<h2>Núcleo de Carreiras</h2>
+            </div> <!-- center wow fadeInDown -->
+            <div class="wow fadeInDown">
+					<div class="col-sm-12">
+						<p>
+                        <fieldset>
+                        	<legend>Conheça o Núcleo de Carreiras</legend>
+                            <div id="divCarreiras" title="Clique aqui para saber mais sobre a Instituição">
+                           		<ul id="div-sobre" class="nav navbar-nav">
+                            		<li><a href="/sobre.php"><label><img src="/imagens/logo-login.png">Conheça mais sobre a Instituição</label></a><br /></li>
+                                </ul>
+                            </div>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vitae erat ultrices, laoreet dui nec, consectetur tellus. In consequat pretium mi id pretium. Phasellus malesuada lectus lacus, in congue metus aliquet sit amet. Aenean et nulla ut massa tincidunt elementum ac at mi. Sed in pretium lectus, ac hendrerit tortor. In hac habitasse platea dictumst. Cras feugiat mattis ante, id aliquam eros varius consectetur. Duis eget finibus erat, vitae viverra elit.
+
+Integer et semper quam, eu vehicula metus. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Pellentesque condimentum facilisis finibus. In posuere velit vel pharetra dignissim. Nullam dictum felis id erat faucibus aliquam. Aenean consequat felis tellus, ut auctor nisl condimentum et. Aliquam id justo tempus, commodo nibh vel, porttitor lectus. Donec pulvinar lobortis ipsum, sed varius erat interdum sed. Praesent vestibulum libero blandit enim cursus, in rutrum eros gravida. Morbi pellentesque eros vel turpis dignissim blandit. Etiam faucibus efficitur odio ut vulputate. Sed molestie purus eget sem mollis scelerisque. Proin cursus laoreet elit, sed pellentesque velit facilisis sodales.
+
+Nullam at consectetur ligula, sed auctor dolor. Phasellus vitae lobortis libero. Nulla eget quam at justo fermentum efficitur. Vestibulum maximus elit in ante lobortis rhoncus. Praesent luctus magna ut enim consectetur aliquet. Nulla mauris ex, accumsan sit amet tempor vitae, sodales eu velit. In hac habitasse platea dictumst. 
+						</fieldset>
+						</p>
+					</div> <!-- col-sm-12 -->
+				</div> <!-- wow fadeInDown -->
+			</div> <!-- container -->
+		</section> <!-- section-sobre-sga -->
+    
+    <!-- include com o section 'bottom' e o footer -->
+    <? include("../includes/server/include-footer.php"); ?> 
+</body>
+</html>
---
+++ servicos/faq.php
@@ -0,0 +1,50 @@
+﻿<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8" />
+    <meta name="author" content="Tiago Henrique, Yasmin Farias, Nyelson Gomes, Renan Oliveira, Ramon Portela, Roberto Souza" /> 
+  	<meta name="keywords" content="faculdade, alunos, home" />
+  	<meta name="description" content="Sistema de Gestão Acadêmica, Avaliações e Administração de Curso em uma Instituição." />
+    <title>SGA | Sobre a Instituição</title>
+    
+    <!-- include com os scripts e CSS do SGA -->
+    <? include("../includes/server/include-css-js-favicon.php"); ?> 
+</head>
+
+    <!-- include com o header, botões sociais, Procurar, logo e Menu -->
+    <? include("../includes/server/include-header.php"); ?> 
+    
+    <section id="section-sobre-sga" >
+        <div class="container">
+        	<div class="center wow fadeInDown">
+            	<h2>FAQ</h2>
+            </div> <!-- center wow fadeInDown -->
+            <div class="wow fadeInDown">
+					<div class="col-sm-12">
+						<p>
+                        <fieldset>
+                        	<legend>Perguntas Frequentes</legend>
+                            <div id="divPergunta1"><label>Fusce luctus elit dui, id sodales nibh consectetur non. Nullam rutrum congue purus et luctus. Proin rhoncus in mauris sed hendrerit. Duis congue eros massa, eget tristique eros scelerisque id?<br /></label>
+                            </div>                        
+                        Nunc at neque mattis, semper nibh et, accumsan leo. Cras quis vulputate ante. Donec lorem dui, tincidunt id efficitur ac, pretium ut nisl?Etiam lacus turpis, molestie id lorem ut, auctor pulvinar erat. Donec sapien dolor, lacinia maximus tortor consequat, dignissim tristique ligula. Duis mattis est eu magna laoreet cursus. Maecenas sit amet congue dui. Mauris consectetur orci lacus, ut porta augue rhoncus quis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut blandit eros non erat bibendum, vel sollicitudin tortor aliquam. In egestas ex quam, vel lobortis dui ullamcorper non.<br /><br />
+                        	
+                        	<div id="divPergunta2"><label>Aliquam eu lorem volutpat, faucibus massa ut, pharetra nunc. Aenean sagittis elit ac consectetur maximus?</label> 
+                            </div>
+ Nunc pulvinar quam rutrum sodales facilisis. Fusce tortor nulla, fermentum ut sem non, fermentum gravida risus. Proin auctor ipsum non tortor sodales ornare. Praesent imperdiet neque non ipsum iaculis lobortis. Sed sodales quis nibh a commodo. In lacinia arcu nulla, ac ornare urna iaculis vitae. Vivamus condimentum risus nisi, elementum condimentum odio volutpat fringilla.<br /><br />
+ 							<div id="divPergunta3"><label>Aenean eget auctor risus. Ut ullamcorper aliquam diam, in rhoncus orci tincidunt ac?</label><br />
+                            </div>
+Nam iaculis dignissim dolor a mollis. Cras rutrum tempor libero, vitae vestibulum nulla hendrerit eu. Aliquam elit mi, tempus sed consequat nec, convallis ac elit. Vestibulum sed enim at tellus faucibus pretium vitae vel dolor. Aliquam erat volutpat. Duis scelerisque mauris id vehicula pharetra. Nulla tempus urna turpis. Nam posuere tellus purus, et egestas neque rutrum sed. <br /><br />
+
+							<div id="divPergunta4"><label>Aliquam velit mauris, tempus ac arcu quis, eleifend sodales ipsum. Nullam accumsan diam vitae vestibulum bibendum. Suspendisse sed nisi sit amet justo dignissim commodo at vitae eros. Quisque faucibus blandit nibh eget blandit?</label><br />
+                            </div> Aliquam ac nisl nibh. Integer feugiat odio at erat elementum, varius vestibulum libero consequat. Aenean porttitor lorem sed felis lacinia, in finibus elit vestibulum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. 
+						</fieldset>
+						</p>
+					</div> <!-- col-sm-12 -->
+				</div> <!-- wow fadeInDown -->
+			</div> <!-- container -->
+		</section> <!-- section-sobre-sga -->
+    
+    <!-- include com o section 'bottom' e o footer -->
+    <? include("../includes/server/include-footer.php"); ?> 
+</body>
+</html>
