b7e5af5f10c906674121ecf6e5eaba452699051a
Ramon Portela <portela.ramon96@gmail.com>
2015-06-05 23:41:25 -0300

    v2.15.3

--- acount/adminal/notas.php
+++ acount/adminal/notas.php
@@ -5,6 +5,49 @@
 		header("Location: /acount/?msg=$msg");
 	}
 	define('TITULO','Notas');
+		$trTemp="";		
+	//Conecção ao Banco de Dados
+	$conexao = @mysql_connect("localhost", "root", "");
+	if (!$conexao) {
+		exit("Site Temporariamente fora do ar");}
+	
+	mysql_select_db("infnetgrid", $conexao);
+	
+	$query = "SELECT modulo.`nome`, avaliacao.`nota`
+			  FROM `aluno_avaliacao` 
+			  JOIN avaliacao ON avaliacao.`idAvaliacao` = aluno_avaliacao.`idAvaliacao`
+			  JOIN modulo ON modulo.`idModulo` = avaliacao.`idModulo`
+			  JOIN aluno ON aluno.`matricula` = aluno_avaliacao.`alunoMatricula`
+			  WHERE aluno.`matricula` = '{$_SESSION['alMatricula']}'";
+	
+	$resultadoPesquisa = @mysql_query($query, $conexao);
+	$msg = "";
+	$numeroPesquisa = @mysql_num_rows($resultadoPesquisa);
+	if ($numeroPesquisa >= 1){
+		$contador = 0;
+		while($avaliacao = mysql_fetch_array($resultadoPesquisa, MYSQL_ASSOC)){
+			$trTemp.="<tbody>
+						 <td>{$avaliacao['nome']}</td>
+						 <td>{$avaliacao['nota']}</td>
+						 <td>8</td>
+						 <td>--</td>
+						 <td>7</td>
+						 <td>Aprovado</td>
+					  </tbody>
+					  ";
+									
+									
+		}
+	} else {
+		$trTemp.="<div class=\"col-md-6\">
+				  		<div class=\"panel panel-info\">
+						<tbody>
+						 <td>Aluno não está cursando nenhum módulo</td>							
+						</div>
+				  </div><!--/.col-->";
+	}
+	
+	mysql_close($conexao);
 ?>
 
 <!DOCTYPE html>
@@ -35,61 +78,9 @@
 				<h1 class="page-header"><?=TITULO?></h1>
 			</div> <!-- col-lg-12 -->
 		</div> <!-- row -->
-        
-		<div class="col-md-12">
-            <div class="panel panel-default"> <!-- panel-heading -->
-                <div class="panel-heading">Ciclo 3</div>
-                <div class="table-responsive panel-body">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th data-field="disciplina" data-align="right">Disciplina</th>
-                                <th data-field="av1">1ª Avaliação</th>
-                                <th data-field="av2">2ª Avaliação</th>
-                                <th data-field="av3">Prova Final</th>
-                                <th data-field="pf">Nota Final</th>
-                                <th data-field="status">Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <td>Discplina #1</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>--</td>
-                            <td>7</td>
-                            <td>Aprovado</td>
-                        </tbody>
-                        <tbody>
-                            <td>Discplina #2</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>--</td>
-                            <td>7</td>
-                            <td>Aprovado</td>
-                        </tbody>
-                        <tbody>
-                            <td>Discplina #3</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>--</td>
-                            <td>7</td>
-                            <td>Aprovado</td>
-                        </tbody>
-                        <tbody>
-                            <td>Discplina #4</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>--</td>
-                            <td>7</td>
-                            <td>Aprovado</td>
-                        </tbody>
-                    </table>
-                </div> <!-- table-responsive panel-body -->
-            </div> <!-- panel panel-default -->
-        </div> <!-- col-md-12 -->
         <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading">Ciclo 2</div> <!-- panel-heading -->
+                <div class="panel-heading">Resumo de notas</div> <!-- panel-heading -->
                 <div class="table-responsive panel-body">
                     <table class="table table-hover">
                         <thead>
@@ -102,89 +93,7 @@
                                 <th data-field="status">Status</th>
                             </tr>
                         </thead>
-                        <tbody>
-                            <td>Discplina #1</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>--</td>
-                            <td>7</td>
-                            <td>Aprovado</td>
-                        </tbody>
-                        <tbody>
-                            <td>Discplina #2</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>--</td>
-                            <td>7</td>
-                            <td>Aprovado</td>
-                        </tbody>
-                        <tbody>
-                            <td>Discplina #3</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>--</td>
-                            <td>7</td>
-                            <td>Aprovado</td>
-                        </tbody>
-                        <tbody>
-                            <td>Discplina #4</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>--</td>
-                            <td>7</td>
-                            <td>Aprovado</td>
-                        </tbody>
-                    </table>
-                </div> <!-- table-responsive panel-body -->
-            </div> <!-- panel panel-default -->
-        </div> <!-- col-md-12 -->
-        <div class="col-md-12">
-            <div class="panel panel-default">
-                <div class="panel-heading">Ciclo 1</div> <!-- panel-heading -->
-                <div class="table-responsive panel-body">
-                    <table class="table table-hover">
-                        <thead>
-                            <tr>
-                                <th data-field="disciplina" data-align="right">Disciplina</th>
-                                <th data-field="av1">1ª Avaliação</th>
-                                <th data-field="av2">2ª Avaliação</th>
-                                <th data-field="av3">Prova Final</th>
-                                <th data-field="pf">Nota Final</th>
-                                <th data-field="status">Status</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <td>Discplina #1</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>--</td>
-                            <td>7</td>
-                            <td>Aprovado</td>
-                        </tbody>
-                        <tbody>
-                            <td>Discplina #2</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>--</td>
-                            <td>7</td>
-                            <td>Aprovado</td>
-                        </tbody>
-                        <tbody>
-                            <td>Discplina #3</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>--</td>
-                            <td>7</td>
-                            <td>Aprovado</td>
-                        </tbody>
-                        <tbody>
-                            <td>Discplina #4</td>
-                            <td>7</td>
-                            <td>8</td>
-                            <td>--</td>
-                            <td>7</td>
-                            <td>Aprovado</td>
-                        </tbody>
+                        <?= $trTemp?>
                     </table>
                 </div> <!-- table-responsive panel-body -->
             </div> <!-- panel panel-default -->
